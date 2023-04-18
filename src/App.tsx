922e29fbf855b01bebff8501ee013271fc50f8fe
Henry Fleitas Mezquia <henryfm1994@gmail.com>
2023-04-18 01:17:13 -0400

    add cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  // Pointer move
+  useEffect(() => {
+    console.log("Efecto", { enabled });
+
+    const handleMove = (event: { clientX: any; clientY: any }) => {
+      const { clientX, clientY } = event;
+      console.log("handleMove", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    // Limpiar el evento anterior del useEffect
+    // Se ejecuta cuanddo el componente se desmonta y cuando
+    //cambian las dependencias antes de ejecutar el efecto nuevamente
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  //Change className Body
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
+
   return (
-    <div className="App">
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
+      />
       <h3>Mouse follower</h3>
-    </div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </main>
   );
 }
 
